76536aa2912661bcd637bc4e9c041e115dd9c695
Basar Ozogut <5684602+basarozogut@users.noreply.github.com>
2022-03-03 00:14:21 +0300

    add breed filter

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 import DogList from './components/DogList';
 import DogGallery from './components/DogGallery';
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container, Row, Col, Form } from 'react-bootstrap';
 import { useEffect, useState } from 'react';
 import api from './api/api';
 
 
 function App() {
   const [dogs, setDogs] = useState([]);
+  const [dogsFiltered, setDogsFiltered] = useState([]);
   const [currentBreed, setCurrentBreed] = useState(null);
+  const [searchInput, setSearchInput] = useState("");
 
   useEffect(() => {
     api.get(`breeds/list/all`)
@@ -18,15 +20,33 @@ function App() {
       });
   }, []);
 
+  useEffect(() => {
+    setDogsFiltered(dogs);
+  }, [dogs]);
+
   function handleDogSelected(breed) {
     setCurrentBreed(breed);
+  }
+
+  function handleOnSearchInputChange(e) {
+    const input = e.target.value;
+    setSearchInput(input);
+    setDogsFiltered(dogs.filter(breed => breed.includes(input)));
   }
 
   return (
     <Container>
       <Row>
         <Col md={3}>
-          <DogList dogs={dogs} onDogSelected={handleDogSelected} />
+          <h3>Doges</h3>
+          <Form.Control
+            placeholder="Doge search"
+            className="mb-3"
+            onChange={handleOnSearchInputChange}
+            value={searchInput} />
+          <DogList
+            dogs={dogsFiltered}
+            onDogSelected={handleDogSelected} />
         </Col>
         <Col md={9}>
           <DogGallery breed={currentBreed} />
--- src/components/DogList.js
+++ src/components/DogList.js
@@ -3,7 +3,6 @@ import { ListGroup } from "react-bootstrap";
 export default function DogList(props) {
     return (
         <div>
-            <h3>Doges</h3>
             {props.dogs.length > 0 &&
                 <ListGroup>
                     {props.dogs.map((dog) =>
